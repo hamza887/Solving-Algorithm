6f4cfcc2c36dc04992adb2580ed1e1585163255f
hamza887 <hamzabutt887>
2021-06-14 04:44:49 -0400

    added cat arrray

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -55,3 +55,12 @@ console.log(wolfy)
     for (const hello of turtles) {
           console.log(hello.toUpperCase(turtles));
     }
+
+    for (let i = 0; i <=20; i++) {
+        if(i%3===0){
+            console.log('human stop taking pics of me');
+        }else{
+            console.log('Love me,pet me !HSSSSSSS')
+        }
+        
+    }
