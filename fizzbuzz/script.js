4889bcd9a19d1724a39b7b0781c59445dcd8d7cf
hamza887 <hamzabutt887>
2021-06-14 03:35:09 -0400

    arrays methods and strign methods

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -15,3 +15,23 @@
    var arr2=[...arr];
 
     console.log(arr2)
+
+    for (let i = 0; i<=20; i++) {
+        if(i%3){
+        console.log(i)
+        }
+        
+    }
+     
+    for (let i = 0; i<=100; i++) {
+        if(i%3===0){
+            console.log('only 3')
+             }else if(i%5===0){
+            console.log('only 5')
+        }else if(i%5===0&& i%3===0){
+            console.log('only 5 and only 3')
+        }else{
+            console.log(i);
+        }
+        
+    }
