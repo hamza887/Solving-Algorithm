85b0fda1e899e13640c64cbce2bdb302c47027c9
hamza887 <39979367+hamza887@users.noreply.github.com>
2021-06-22 03:01:14 -0400

    Adding some new expresiion and functions

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -64,3 +64,14 @@ console.log(wolfy)
         }
         
     }
+
+//function returns true if and only if x is equal to 7.
+
+function isSeven(x) {
+	return x=="7"?true:false;
+}
+
+console.log(isSeven(9)); 
+
+console.log(isSeven(7))
+
