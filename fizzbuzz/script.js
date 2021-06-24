526fa71fb8276f3731784b9feee9a8feece063cb
hamza887 <39979367+hamza887@users.noreply.github.com>
2021-06-24 13:46:11 -0400

    adding some new algorthms

--- fizzbuzz/script.js
+++ fizzbuzz/script.js
@@ -75,3 +75,18 @@ console.log(isSeven(9));
 
 console.log(isSeven(7))
 
+
+//At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
+
+//You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers only, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
+
+
+
+function ss(hello){
+    const Maximumnum = Math.max(...hello);
+     const Minumumnum= Math.min(...hello);
+  return [Minumumnum, Maximumnum, (Maximumnum-Minumumnum)];
+
+}
+
+console.log(ss([82, 15, 6, 38, 35]))
